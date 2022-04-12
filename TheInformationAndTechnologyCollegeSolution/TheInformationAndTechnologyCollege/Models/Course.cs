401317f82597231d343931ea0d309fb7c5015cb9
aqeel26 <moh9726@hotmail.com>
2022-04-12 15:35:16 +0100

    Mo

---
+++ TheInformationAndTechnologyCollegeSolution/TheInformationAndTechnologyCollege/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheInformationAndTechnologyCollege.Models
+{
+    public class Course
+    {
+        public virtual int CourseID { get; set; }
+        public virtual string CourseName { get; set; }
+    }
+}
---
+++ TheInformationAndTechnologyCollegeSolution/TheInformationAndTechnologyCollege/Models/Module.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheInformationAndTechnologyCollege.Models
+{
+    public class Module
+    {
+            //is genrate the database code based on these propeties automatically
+            public virtual int ModuleID { get; set; }
+            public virtual string ModuleCode { get; set; }
+            public virtual string ModulTitle { get; set; }
+            public virtual string ModuleDescription { get; set; }
+            public virtual string ModuleContents { get; set; }
+            public virtual Course Course { get; set; }
+
+        
+    }
+}
