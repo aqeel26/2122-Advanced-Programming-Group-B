2471ce53334aff7cc1aaaef50bad7eea09bb19ea
Sukhman991007 <97964094+Sukhman991007@users.noreply.github.com>
2022-04-14 23:28:12 +0100

    Sukhman 123

---
+++ TheInformationTechnologyCollege/TheInformationTechnologyCollege/Models/StudentRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheInformationTechnologyCollege.Models
+{
+    public class StudentRegistration
+    {
+        public virtual int StudentRegistrationID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string City { get; set; }
+        public virtual string PostCode { get; set; }
+
+        public virtual Course course { get; set; }
+    }
+}
